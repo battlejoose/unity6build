eb9f29cb48d21f68f2999313ee1b4e9901285656
battlejoose <battlejoose@gmail.com>
2025-09-02 10:02:39 -0700

    trying to log balance

--- public/Build/public.loader.js
+++ public/Build/public.loader.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a9e896205cf270e0cf8cf754dde04563a3d11ace77f7c854a9519d8466e90ef
+oid sha256:48d032f73eee647770c9bc868cc8e5ff422b0a06a18b75ed769b2d6531bc49ab
 size 26454
